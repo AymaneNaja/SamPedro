6d9316d84ca9b317bc86ae09ba98ed17533afad3
fun time <mrjgaming4@gmail.com>
2025-01-26 14:50:40 +0400

    v2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+// Define a type for the global object with the `prisma` property
+declare global {
+    var prisma: PrismaClient | undefined;
+}
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-} else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient();
-    }
-    prisma = global.prisma;
+// Initialize Prisma Client
+const prisma: PrismaClient = global.prisma || new PrismaClient();
+
+// Store the Prisma Client in the global object in development
+if (process.env.NODE_ENV !== 'production') {
+    global.prisma = prisma;
 }
 
 export default prisma;
