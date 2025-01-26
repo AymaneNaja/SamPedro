19a61f71e4b7ade31dbfae8ed8543ccc240d5989
fun time <mrjgaming4@gmail.com>
2025-01-26 14:59:20 +0400

    v3

--- auth.ts
+++ auth.ts
@@ -5,6 +5,10 @@ import { verify } from "argon2"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GitHubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaClient } from "@prisma/client"
+
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
